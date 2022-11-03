1ee6e4d857f690b07a9bdcb14e8a5ff6d1ceeb11
Mirko <mirkopoj@hotmail.com>
2022-11-03 12:40:09 -0300

    Servidor en orden

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,12 @@
-use std::thread;
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write, ErrorKind};
+use std::net::TcpListener;
 use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, Sender, TryRecvError, RecvTimeoutError};
-use std::time::Duration;
+use std::sync::mpsc::Sender;
 
 extern crate modulos_comunes;
-use modulos_comunes::{TcpMessage, EMPTYTCPMESSAGE};
+use modulos_comunes::TcpMessage;
 
-fn handle_client(mut stream: TcpStream, tx:Sender<TcpMessage>, rx: Receiver<TcpMessage>) {
-    let (sub_tx, sub_rx) = mpsc::channel();
-    let stream_clone = stream.try_clone().expect("stream clone fail");
-    thread::spawn(move|| {
-        handle_subclient(stream_clone, sub_tx)
-    });
-
-    let mut hay_cliente = true;
-    while hay_cliente {
-        //mensajes entrantes del cliente
-        match sub_rx.recv_timeout(Duration::new(1,0)) {
-            Ok(msg) => {
-                tx.send(msg).expect("sub_tx failed");
-            },
-            Err(why) => {
-                if why == RecvTimeoutError::Disconnected{
-                    println!("reader_tx terminated");
-                    hay_cliente = false;
-                } 
-            }
-        };
-
-        //mensajes salientes del server
-        match rx.try_recv() {
-            Ok(data) => {
-                match stream.write(&data) {
-                    Ok(_) => { },
-                    Err(why) => { println!("stream write failed {}", why); },
-                };
-            },
-            Err(why) => {
-                if why != TryRecvError::Empty {
-                    println!("An error occurred, rx client");
-                    hay_cliente = false;
-                }
-            }
-        } 
-    }
-
-    drop(stream);
-}
-
-fn handle_subclient(mut stream: TcpStream, sub_tx: Sender<TcpMessage>) {
-    let addr = stream.peer_addr().expect("Stream peer_addr failed on subclient");
-    let mut data: TcpMessage = EMPTYTCPMESSAGE; // using 50 byte buffer
-    while match stream.read(&mut data) {
-        Ok(size) => {
-            if size==0 { 
-                println!("Connection with {}, closed", addr);
-                false 
-            } else {
-                sub_tx.send(data).expect("tx subclient, falló");
-                true
-            }
-        },
-        Err(why) => {
-            if why.kind().eq(&ErrorKind::WouldBlock) { 
-                true 
-            }
-            else {
-                println!("An error occurred, terminating connection with {}", addr);
-                false
-            }
-        }
-    } {}
-}
-
-fn listener_launch(listener: TcpListener, client_tx: Sender<TcpMessage>, sender_tx: Sender<Sender<TcpMessage>>) {
-    thread::spawn( move || {
-        for stream in listener.incoming() {
-            match stream {
-                Ok(stream) => {
-                    println!("New connection: {}", stream.peer_addr().expect("Stream peer_addr failed on new connection"));
-                    let (server_tx, client_rx) = mpsc::channel();
-                    let txcli = client_tx.clone();
-                    sender_tx.clone().send(server_tx).expect("sendersender fail");
-                    thread::spawn(move|| {
-                        // connection succeeded
-                        handle_client(stream, txcli, client_rx)
-                    });
-                }
-                Err(e) => {
-                    println!("Error: {}", e);
-                    /* connection failed */
-                }
-            }
-        }
-        // close the socket server
-        drop(listener);
-    });
-}
+mod server;
+use crate::server::*;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:3333").expect("listener failed on bind");
@@ -110,51 +18,11 @@ fn main() {
     listener_launch(listener, client_tx, sender_tx);
 
     loop {
-        loop {
-            //Nuevas coneciones
-            match sender_rx.try_recv() {
-                Ok(sender) => {
-                    txs.push(sender);
-                },
-                Err(why) => {
-                    if why == TryRecvError::Empty{
-                        break;
-                    } else {
-                        panic!("reader_tx terminated");
-                    }
-                }
-            };
-        }
+        recibir_conecciones_nuevas(&sender_rx, &mut txs);
 
-        //Lectura de los clientes
-        let data: TcpMessage = match server_rx.try_recv() {
-            Ok(msg) => {
-                msg
-            },
-            Err(why) => {
-                if why == TryRecvError::Empty{
-                    EMPTYTCPMESSAGE
-                }
-                else {
-                    panic!("server_rx failed");
-                }
-            }
-        };
+        let data = leer_clientes(&server_rx);
 
-        //Escritura a los clientes
-        if data != EMPTYTCPMESSAGE {
-            let mut index = 0;
-            let txsc = txs.clone();
-            for tx in txsc {
-                match tx.send(data){
-                    Ok(_) => { },
-                    Err(_) => {
-                        txs.remove(index);
-                    },
-                };
-                index += 1;
-            }
-        }
+        escribir_clientes(data, &mut txs);
     }
 
 }
---
+++ src/server.rs
@@ -0,0 +1,151 @@
+use std::thread;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write, ErrorKind};
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender, TryRecvError, RecvTimeoutError};
+use std::time::Duration;
+
+extern crate modulos_comunes;
+use modulos_comunes::{TcpMessage, EMPTYTCPMESSAGE};
+
+fn handle_client(mut stream: TcpStream, tx:Sender<TcpMessage>, rx: Receiver<TcpMessage>) {
+    let (sub_tx, sub_rx) = mpsc::channel();
+    let stream_clone = stream.try_clone().expect("stream clone fail");
+    thread::spawn(move|| {
+        handle_subclient(stream_clone, sub_tx)
+    });
+
+    let mut hay_cliente = true;
+    while hay_cliente {
+        //mensajes entrantes del cliente
+        match sub_rx.recv_timeout(Duration::new(1,0)) {
+            Ok(msg) => {
+                tx.send(msg).expect("sub_tx failed");
+            },
+            Err(why) => {
+                if why == RecvTimeoutError::Disconnected{
+                    println!("reader_tx terminated");
+                    hay_cliente = false;
+                } 
+            }
+        };
+
+        //mensajes salientes del server
+        match rx.try_recv() {
+            Ok(data) => {
+                match stream.write(&data) {
+                    Ok(_) => { },
+                    Err(why) => { println!("stream write failed {}", why); },
+                };
+            },
+            Err(why) => {
+                if why != TryRecvError::Empty {
+                    println!("An error occurred, rx client");
+                    hay_cliente = false;
+                }
+            }
+        } 
+    }
+
+    drop(stream);
+}
+
+fn handle_subclient(mut stream: TcpStream, sub_tx: Sender<TcpMessage>) {
+    let addr = stream.peer_addr().expect("Stream peer_addr failed on subclient");
+    let mut data: TcpMessage = EMPTYTCPMESSAGE; // using 50 byte buffer
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            if size==0 { 
+                println!("Connection with {}, closed", addr);
+                false 
+            } else {
+                sub_tx.send(data).expect("tx subclient, falló");
+                true
+            }
+        },
+        Err(why) => {
+            if why.kind().eq(&ErrorKind::WouldBlock) { 
+                true 
+            }
+            else {
+                println!("An error occurred, terminating connection with {}", addr);
+                false
+            }
+        }
+    } {}
+}
+
+pub fn listener_launch(listener: TcpListener, client_tx: Sender<TcpMessage>, sender_tx: Sender<Sender<TcpMessage>>) {
+    thread::spawn( move || {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    println!("New connection: {}", stream.peer_addr().expect("Stream peer_addr failed on new connection"));
+                    let (server_tx, client_rx) = mpsc::channel();
+                    let txcli = client_tx.clone();
+                    sender_tx.clone().send(server_tx).expect("sendersender fail");
+                    thread::spawn(move|| {
+                        // connection succeeded
+                        handle_client(stream, txcli, client_rx)
+                    });
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                    /* connection failed */
+                }
+            }
+        }
+        // close the socket server
+        drop(listener);
+    });
+}
+
+pub fn leer_clientes(server_rx: &Receiver<TcpMessage>) -> TcpMessage {
+    match server_rx.try_recv() {
+        Ok(msg) => {
+            msg
+        },
+        Err(why) => {
+            if why == TryRecvError::Empty{
+                EMPTYTCPMESSAGE
+            }
+            else {
+                panic!("server_rx failed");
+            }
+        }
+    }
+}
+
+pub fn recibir_conecciones_nuevas(sender_rx: &Receiver<Sender<TcpMessage>>, txs: &mut Vec<Sender<TcpMessage>>) {
+    loop {
+        match sender_rx.try_recv() {
+            Ok(sender) => {
+                txs.push(sender);
+            },
+            Err(why) => {
+                if why == TryRecvError::Empty{
+                    break;
+                } else {
+                    panic!("reader_tx terminated");
+                }
+            }
+        };
+    }
+}
+
+pub fn escribir_clientes(data: TcpMessage, txs: &mut Vec<Sender<TcpMessage>>) {
+    if data != EMPTYTCPMESSAGE {
+        let mut index = 0;
+        let txsc = txs.clone();
+        for tx in txsc {
+            match tx.send(data){
+                Ok(_) => { },
+                Err(_) => {
+                    txs.remove(index);
+                },
+            };
+            index += 1;
+        }
+    }
+}
+
